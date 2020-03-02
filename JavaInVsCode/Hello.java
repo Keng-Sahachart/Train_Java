bea88642f995d982707fac4c6f03f031788231b5
Keng-Sahachart <sahachart.p19@gmail.com>
2020-03-03 01:50:24 +0700

    ReRun
    ReRun

---
+++ JavaInVsCode/Hello.java
@@ -0,0 +1,13 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("World");
+        
+        int age = 4 ;
+        if (age > 5) {
+            System.out.println("Upper");
+        } else {
+            System.out.println("Lower");
+        }
+    }
+}
